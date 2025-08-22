d05d18991a2de9863f84cb89bf9bbad9b548133c
Matt <mateuszmalinowski7777@gmail.com>
2025-08-22 10:01:53 +0200

    Delete product for category added

---
+++ Shop.Application/Products/Commands/DeleteProduct/DeleteProductForCategoryCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using Shop.Application.Brands.Queries.GetAllBrandsForCategory;
+using Shop.Domain.Entities;
+using Shop.Domain.Exceptions;
+using Shop.Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Application.Products.Commands.DeleteProduct
+{
+    public class DeleteProductForCategoryCommandHandler(ILogger<DeleteProductForCategoryCommandHandler> logger,
+        IProductsRepository productsRepository) : IRequestHandler<DeleteProductForCategoryCommand>
+    {
+        public async Task Handle(DeleteProductForCategoryCommand request, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("Deleting product for category with id:{categoryId}",
+                request.categoryId);
+
+            var category = await productsRepository.GetAllProductsForCategory(request.categoryId)
+                ?? throw new NotFoundException(nameof(Category), request.categoryId.ToString());
+
+            var product = category.Products.FirstOrDefault(p => p.Id == request.productId);
+
+            await productsRepository.DeleteProduct(product!);
+        }
+    }
+}
---
+++ Shop.ApplicationTests/Products/Commands/CreateProduct/CreateProductForCategoryIdAndSubcateoryIdWithBrandIdCommandValidatorTests.cs
@@ -0,0 +1,64 @@
+﻿using FluentValidation.TestHelper;
+using Xunit;
+
+
+namespace Shop.Application.Products.Commands.CreateProduct.Tests
+{
+    public class CreateProductForCategoryIdAndSubcateoryIdWithBrandIdCommandValidatorTests
+    {
+        [Fact()]
+        public void CreateProductValidator_ForValidCreateCommand_ShouldNotHaveValidationErrors()
+        {
+            // arrange
+
+            var command = new CreateProductForCategoryIdAndSubcateoryIdWithBrandIdCommand()
+            {
+                Name = "Test",
+                Description = "Something about product",
+                IsAvailable = true,
+                Cost = 9.9999f,
+                StockQuantity = 999,
+                CategoryId = 10,
+                SubCategoryId = 8,
+                BrandId = 10
+            };
+
+            var validator = new CreateProductForCategoryIdAndSubcateoryIdWithBrandIdCommandValidator();
+
+            // act 
+
+            var result = validator.TestValidate(command);
+
+            // assert 
+
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+        
+        [Fact()]
+        public void CreateProductValidator_ForInvalidCreateCommand_ShouldHaveValidationErrors()
+        {
+            // arrange
+            var validator = new CreateProductForCategoryIdAndSubcateoryIdWithBrandIdCommandValidator();
+
+            var command = new CreateProductForCategoryIdAndSubcateoryIdWithBrandIdCommand()
+            {
+                Name = "T",
+                Description = "Some",
+                IsAvailable = true,
+                Cost = 99999f,
+                StockQuantity = 0,
+                CategoryId = 0,
+                SubCategoryId = 0,
+                BrandId = 0
+            };    
+
+            // act 
+
+            var result = validator.TestValidate(command);
+
+            // assert 
+
+            result.ShouldHaveValidationErrors();
+        }
+    }
+}
